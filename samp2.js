d82d3ffcf4125f681cde4d4c45767462a886c3c1
mischeif808 <articulatemischief@gmail.com>
2023-01-05 21:59:36 +0530

    OK

---
+++ samp2.js
@@ -0,0 +1,15 @@
+var a=10;
+var b=20.5;
+var c=30;
+var d="Back";
+var e="Ground";
+var f=true;
+var g=false;
+var disc;
+console.log(a*b);
+console.log(a*c);
+console.log(a*d);
+console.log(a*e);
+console.log(a*f);
+console.log(a*g);
+console.log(d*e);

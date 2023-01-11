555e5b0ec7b8da97d1d9cfefbfee2e800b6788bd
mischeif808 <articulatemischief@gmail.com>
2023-01-11 08:12:58 +0530

    done

---
+++ Programs2/Examp14.js
@@ -0,0 +1,14 @@
+var n=153;
+var k=786;
+p=(1*3)+(5*3)+(3*3);
+q=(7*3)+(8*3)+(6*3);
+console.log(p);
+console.log(q);
+if(n==p)
+console.log(n," is a Armstrong Number.");
+else 
+console.log(n,"is not a Armstrong number.");
+if(k==q)
+console.log(k," is a Armstrong Number.");
+else 
+console.log(k,"is not a Armstrong number.");

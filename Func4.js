968e58d43ea82048c2835e3c7140b075def4c8d1
mischeif808 <articulatemischief@gmail.com>
2023-01-12 08:14:04 +0530

    done

--- Func4.js
+++ Func4.js
@@ -3,7 +3,7 @@ function cal_exp(join)
   return 2023-join;
 }
 let join=cal_exp(2015);
-console.log(join);
+console.log("Your Experience in Field is",join,"years");
 
 let join1=cal_exp(2012);
-console.log(join1);
+console.log("Your Experience in Field is",join1,"years");
--- Funs2.js
+++
@@ -1,5 +0,0 @@
-function add(a,b)
-{
-        a+b;
-}
-add(45,76);

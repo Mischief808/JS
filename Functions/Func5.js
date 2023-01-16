be06ab2ab367cc1cf7d67015cff2855a477cca91
mischeif808 <articulatemischief@gmail.com>
2023-01-16 21:49:37 +0530

    Done

---
+++ Functions/Func5.js
@@ -0,0 +1,25 @@
+function outer()
+{
+      console.log("This is outside function");
+ 
+    function inner()
+       {
+            console.log("This is inner function");
+          function toinner()
+       {
+            console.log("This is inside inner function");
+        }
+       toinner();
+        }
+        inner();
+}
+outer();
+ function sub()
+{
+console.log("Subtraction");
+}
+sub();
+sub();
+sub();
+sub();
+
---
+++ Functions/Func6.js
@@ -0,0 +1,9 @@
+console.log("This is nesting function");
+var i=1;
+function nest()
+{
+console.log("Nesting Function",i);
+i++;
+nest();
+}
+nest(); 			 

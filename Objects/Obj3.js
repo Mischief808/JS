a1be2408b6b590a4f95974aa27d2cc3332b9d519
mischeif808 <articulatemischief@gmail.com>
2023-01-18 22:39:26 +0530

    done

---
+++ Objects/Obj3.js
@@ -0,0 +1,13 @@
+let size=[38,40,42,44,true,false];
+let emp={id:101,name:"Ramesh",gender:"Male"};
+console.log(typeof(size));
+console.log(typeof(emp));
+console.log(emp.name);
+for(e in emp)
+{
+  console.log(e);
+}
+for(s of size)
+{
+console.log(s);
+}

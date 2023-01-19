62c3d412d0c600eab53542014591426ebd25f35e
mischeif808 <articulatemischief@gmail.com>
2023-01-19 23:09:02 +0530

    Done

---
+++ Objects/ObjLen1.js
@@ -0,0 +1,15 @@
+let car={};
+/*
+if(Object.keys(car)==0)
+{
+console.log("It is Empty Object");
+}
+else
+{
+console.log("It is Not Empty Object");
+}
+*/
+
+Object.keys(car)==0?console.log("Empty Object"):console.log("Not Empty Object")
+
+

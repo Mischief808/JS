fbb3c4f80048d0c7581eecddc517f8635bf9a281
mischeif808 <articulatemischief@gmail.com>
2023-01-19 22:55:10 +0530

    Done

---
+++ Objects/ArrMeth1.js
@@ -0,0 +1,8 @@
+let brands=[];
+console.log(brands.length);
+brands.push("Allen Solly");
+console.log(brands);
+brands.push("PeterEngland");
+console.log(brands);
+brands.push("TommyHilfiger");
+console.log(brands);
---
+++ Objects/ObjLen.js
@@ -0,0 +1,4 @@
+let bike={name:"Apache TVS",CC:"180cc"};
+console.log(Object.keys(bike));
+console.log(Object.values(bike));
+console.log(Object.entries(bike));

72a74b1a766ef7794ee4f8d4abb9734cb1870b05
mischeif808 <articulatemischief@gmail.com>
2023-01-19 10:59:57 +0530

    done

---
+++ Objects/Obj4.js
@@ -0,0 +1,27 @@
+let dat=[{id:101,name:"Gibran",loc:"Banglore"},{id:102,name:"Daniel",loc:"Noida"},{id:103,name:"Michael",loc:"Hyderabad"}];
+/*
+for(db of dat)
+{
+console.log(db.id);
+console.log(db);
+}
+
+for(i=0;i<=dat.length-1;i++)
+{
+
+console.log(dat[i]);
+}
+
+var i=0;
+while(i<=dat.length-1)
+{
+console.log(dat[i]);
+i++;
+}
+*/
+var i=0;
+do
+{
+console.log(dat[i]);
+i++;
+}while(i<=dat.length-1);

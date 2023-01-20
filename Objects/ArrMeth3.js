9efa6a28868ad3df9345481c6f5b39dedff51129
mischeif808 <articulatemischief@gmail.com>
2023-01-20 08:01:29 +0530

    Done

---
+++ Objects/ArrMeth3.js
@@ -0,0 +1,20 @@
+let n=[4,6,8,3];
+console.log("The length of Array is ",n.length);
+console.log(n);
+n.unshift(5);
+console.log("After Unshifting");
+console.log(n);
+console.log("After Unshifting");
+n.unshift(2);
+console.log(n);
+console.log("After Pushing");
+n.push(1);
+console.log(n);
+console.log("After Shifting");
+n.shift();
+console.log(n);
+console.log("After Shifting");
+n.shift();
+console.log(n);
+
+

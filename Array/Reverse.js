f9b5356c9e485ef7f617ca0cccc66569711a130b
mischeif808 <articulatemischief@gmail.com>
2023-01-24 07:55:05 +0530

    Reverse Array Elements

---
+++ Array/Reverse.js
@@ -0,0 +1,8 @@
+const Super_bikes=["S1000RR","Ninja H2R","Zx10R","HayaBusa"];
+console.log(Super_bikes);
+for(i=4;i<=0;i--)
+{
+  console.log(Super_bikes[i]);
+}
+Super_bikes.reverse();
+console.log(Super_bikes);

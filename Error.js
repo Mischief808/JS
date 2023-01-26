37555940b38220fb7664bc653e6adb6a1140dc9f
mischeif808 <articulatemischief@gmail.com>
2023-01-26 14:02:35 +0530

    Error Method

---
+++ Error.js
@@ -0,0 +1,13 @@
+var a=6;
+var b=6;
+if(a==b)
+{
+console.log("a is equal to b");
+}
+else 
+console.error("Error! a is not equal to b");
+const spacing = '10px';
+  const styles =
+        `padding: ${spacing}; background-color: white; color: green; font-style:
+         italic; border: 1px solid black; font-size: 2em;`;
+  console.log('%cGeeks for Geeks', styles);

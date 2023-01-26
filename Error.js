51858f9a9ac939e75463689463ed899d69382d8c
mischeif808 <articulatemischief@gmail.com>
2023-01-26 14:03:56 +0530

    Erroe Method

--- Error.js
+++ Error.js
@@ -6,8 +6,3 @@ console.log("a is equal to b");
 }
 else 
 console.error("Error! a is not equal to b");
-const spacing = '10px';
-  const styles =
-        `padding: ${spacing}; background-color: white; color: green; font-style:
-         italic; border: 1px solid black; font-size: 2em;`;
-  console.log('%cGeeks for Geeks', styles);

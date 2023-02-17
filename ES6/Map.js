ceae924066c22025402f37a9f41bd34c8b7dc8ef
mischeif808 <articulatemischief@gmail.com>
2023-02-17 15:37:36 +0530

    Map Method example

---
+++ ES6/Map.js
@@ -0,0 +1,14 @@
+let bikes=["kawasaki","suzuki","BMW","KTM"];
+/*
+for(bike of bikes)
+{
+ console.log(bike);
+}
+
+bikes.map((bike)=>{ console.log(bike)})
+bikes.forEach((bikes)=>{  console.log(bikes) })*/
+let names=["Ramesh","Suresh","Satwik","Yarab"];
+console.log("Using ForEach Method");
+names.forEach((names)=>{console.log(names)})
+console.log("Using Map Method");
+names.map((names)=>{console.log(names)})

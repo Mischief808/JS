8ebf54f9eff3fa5989828ad3cbb1a7c99ecf01ee
mischeif808 <articulatemischief@gmail.com>
2023-02-17 15:39:16 +0530

    Rest operator exampple

---
+++ ES6/Rest.js
@@ -0,0 +1,5 @@
+let cars=["Jaguar","Land Rover","Porsche","BMW"];
+console.log(cars);
+console.log("Using Rest Operator");
+let cars_1=[...cars,"Ferrari","Lamborghini",...cars];
+console.log(cars_1);

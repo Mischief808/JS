c746523c1bca93f779b6af734e4794e893eb721c
mischeif808 <articulatemischief@gmail.com>
2023-02-22 15:37:39 +0530

    Pow,Min,Max Functions

--- Others/Math/Math2.js
+++ Others/Math/Math2.js
@@ -1,3 +1,3 @@
-console.log(Math.min(4,2,7,9));
-console.log(Math.pow(8,3));
-console.log(Math.max(6,3,8,1));
+console.log(Math.min(4,2,7,9));//Math Function to find Minimum of given numbers
+console.log(Math.pow(8,3));//Math Function to find powers 
+console.log(Math.max(6,3,8,1));//Function to find maximum of given numbers

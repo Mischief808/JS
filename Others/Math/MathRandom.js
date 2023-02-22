40ed8cafb666d2a64247024ff50fba10a0642a96
mischeif808 <articulatemischief@gmail.com>
2023-02-22 15:50:06 +0530

    Random Function Example

---
+++ Others/Math/MathRandom.js
@@ -0,0 +1,8 @@
+//Generates a random number from 0 to 1
+console.log(Math.random());
+console.log(Math.random());
+//To generate a random number from 0 to 10 
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+

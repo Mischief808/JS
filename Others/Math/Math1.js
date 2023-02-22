b346647f416446e8379a02a66ba714dd4a1c65b3
mischeif808 <articulatemischief@gmail.com>
2023-02-22 15:28:44 +0530

    Math Functions

--- Others/Math/Math1.js
+++ Others/Math/Math1.js
@@ -1,6 +1,6 @@
 console.log("Some of Math Functions");
 console.log(Math.PI);
-console.log(Math.trunc(Math.PI));
+console.log(Math.trunc(4.6748437828));//Truncates the decimals.
 console.log(Math.floor(Math.PI));
-console.log(Math.round(6.7));
-console.log(Math.floor(-7.86));
+console.log(Math.round(6.4));//Rounds off to the nearest Value
+console.log(Math.floor(7.99));//Rounds to the minimum of given Number

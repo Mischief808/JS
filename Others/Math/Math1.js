16c1ecdf97d8484f810310859bc95093b3f1fcdd
mischeif808 <articulatemischief@gmail.com>
2023-02-22 15:08:45 +0530

    Some of Math Functions Examples

---
+++ Others/Math/Math1.js
@@ -0,0 +1,6 @@
+console.log("Some of Math Functions");
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.floor(Math.PI));
+console.log(Math.round(6.7));
+console.log(Math.floor(-7.86));

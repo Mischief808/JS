9c9b1bd947387c71a8ae1e95ff8e91a4ceb60a89
mischeif808 <articulatemischief@gmail.com>
2023-02-23 10:17:43 +0530

    To generate random letters

---
+++ Others/Math/Math3.js
@@ -0,0 +1,13 @@
+/* console.log("uday".charAt(Math.floor(Math.random()*4)));
+console.log("uday".charAt(Math.floor(Math.random()*4)));
+console.log("uday".charAt(Math.floor(Math.random()*4)));
+console.log("uday".charAt(Math.floor(Math.random()*4)));
+ */
+var s="Davij"
+console.log(s.charAt(Math.floor(Math.random()*s.length)));
+console.log(s.charAt(Math.floor(Math.random()*s.length)));
+console.log(s.charAt(Math.floor(Math.random()*s.length)));
+console.log(s.charAt(Math.floor(Math.random()*s.length)));
+console.log(s.charAt(Math.floor(Math.random()*s.length)));
+
+
